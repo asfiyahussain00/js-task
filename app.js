ac6f411e354119ea5e922309ee4a19cd67333e71
asfiya hussain <zk724971@gmail.com>
2023-04-15 13:47:53 -0700

    js assignment

---
+++ app.js
@@ -0,0 +1,109 @@
+    //task no 1
+// zakat value 2.5% (step:1)
+//var zakat_percentage = 0.025; //snake_case
+
+// take the input from the user (step:1 & 3)
+//var user_input =+ prompt("enter your Zakat amount");
+
+
+//  Result = multiple of the zakat percentage and user input (step:4)
+//var Result = (zakat_percentage) * (user_input);
+
+
+// show a result (step:5)
+//var Result = ('Your zakat value is ' + Result);
+//alert(Result);
+
+    //task no 2
+    //user input+
+//var userinput1=+prompt("how many members in your family");
+//var userinput2=prompt("wheat,dates,balary,raisins");
+//fitra amount
+//var wheat=170
+//var balary=560
+//var dates=1600
+//var raisins=2800
+//calculation
+//if (userinput2=='wheat') {
+   // var total = userinput1*wheat
+   // alert( total)}
+//if (userinput2=='balary') {
+   // var total = userinput1*balary
+    //alert( total)}
+//if (userinput2=='dates') {
+   // var total = userinput1*dates
+   // alert( total)}
+//if (userinput2=='raisins') {
+   // var total = userinput1*raisins
+   // alert( total)}
+
+  //task no 3
+ // var secretnumber=6
+//var num= +prompt(" guess the secret number")
+
+//if (num >=7) {
+  // alert ("sorry try again your entered no is high")} 
+
+//if (num <=5) {
+   // alert ("sorry try again your entered no is low")}
+//if (num==secretnumber) {
+  // alert ("congrats you guess right number")}
+
+   // task no 4
+   
+//var a=prompt("enter your name")
+
+//var username= "asfiya";
+//var username = (username.charAt(0).toUpperCase()+username.slice(1));
+
+//alert(username);
+
+   //task no 5
+   
+//userinput
+
+//var contactname =[];
+//var contactnumber =[];
+
+//var input1=prompt("enter your name")
+//var input2=prompt("enter your number")
+
+
+//contactname.push('name')
+//contactnumber.push('number')
+
+
+
+//alert(input1+input2 )
+
+
+    //task no 6 
+    
+//array
+//var product=['pizza', 'burger','fries','roll']
+//userinput
+//var userinput = +prompt("please enter the position of product which you remove ")
+//var a =product.splice(userinput)
+//alert( "remove items:"+product)
+//result
+//delete product[a]
+//remainingitems=(a)
+//alert("remaining items"+remainingitems)
+
+     //  task no 7
+
+ //var nationality =  prompt("enter your nationality")
+//var  gender =  prompt (" enter your gender")
+//var age = prompt( " enter your age")
+     
+ //if (nationality== 'pakistan', 'indian' && gender =='male','female' && age >=18) {alert("you are eligible to vote")} //else {gender =='female'&& age <=18} {alert("your are not eligible to vote")} 
+//if(gender=='female'&& age >=18 )  {var married= prompt("married or unmarried")}// else {alert("your eligible to vote") }
+
+  //task no 8
+  
+  //var cricketplayer=['Babar Azam','Shadab Khan', 'Asif Ali', 'Fakhar Zaman',' Haider Ali', 'Haris Rauf', 'Iftikhar Ahmed', 'Khushdil Shah', 'Mohammad Hasnain',' Mohammad Nawaz', 'Mohammad Rizwan', 'Mohammad Wasim',' Naseem Shah','Shaheen Shah Afridi',' Shan Masood']
+//alert  (cricketplayer)
+//var  selectedplayer= cricketplayer.slice(0,10)
+//alert (selectedplayer)
+ 
+
